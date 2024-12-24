0111f14c21305e9a8b4db66e5f0534f0b2a3e114
Mohannad Nasreldin <mohannadnasr.20@gmail.com>
2024-12-24 05:23:05 +0200

    import verfication

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,70 +1,143 @@
 import React from 'react';
-import { FaFacebook } from 'react-icons/fa';
-import { FaTelegram } from 'react-icons/fa';
-import { FaWhatsapp } from 'react-icons/fa';
-import { FaPhoneAlt } from 'react-icons/fa';
+import { FaFacebook, FaTelegram, FaWhatsapp, FaPhoneAlt } from 'react-icons/fa';
+
 const Footer = () => {
   return (
     <footer className="bg-green-900 text-white py-12 px-6 md:px-20" dir="rtl">
-      <div className="max-w-5xl mx-auto flex flex-col items-center space-y-8 md:space-y-0">
+      <div className="max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-12">
 
         {/* Contact Info Section */}
-        <div className="text-center">
-          <h3 className="text-xl font-semibold text-yellow-300 mb-4">معلومات الاتصال</h3>
+        <div className="text-center md:text-right">
+          <h3 className="text-xl font-semibold text-center text-yellow-300 mb-4">معلومات الاتصال</h3>
           <p className="text-gray-300">
             للتواصل معنا: يمكنكم إرسال رسالة عبر البريد الإلكتروني أو متابعتنا على وسائل التواصل الاجتماعي.
           </p>
           <div className="flex justify-center space-x-6 rtl:space-x-reverse mt-4">
-          <a
-            href="https://t.me/+OBhoac22F3g1Y2M0"
-            className="text-blue-300 hover:scale-110 transition-transform duration-300"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Visit our Telegram page"
-          >
-            <FaTelegram size={40} />
-          </a>
-          <a
-            href="https://chat.whatsapp.com/F18u5uuJHZpEIzjCrnLC9t"
-            className="text-green-500 hover:scale-110 transition-transform duration-300"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Visit our WhatsApp group"
-          >
-            <FaWhatsapp size={40} />
-          </a>
-          <a
-            href="https://www.facebook.com/profile.php?id=61569368545936"
-            className="text-blue-300 hover:scale-110 transition-transform duration-300"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Visit our Facebook page"
-          >
-            <FaFacebook size={40} />
-          </a>
-        </div>
+            <a
+              href="https://t.me/+OBhoac22F3g1Y2M0"
+              className="text-yellow-300 hover:scale-110 transition-transform duration-300"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Visit our Telegram page"
+            >
+              <FaTelegram size={40} />
+            </a>
+            <a
+              href="https://chat.whatsapp.com/F18u5uuJHZpEIzjCrnLC9t"
+              className="text-yellow-300 hover:scale-110 transition-transform duration-300"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Visit our WhatsApp group"
+            >
+              <FaWhatsapp size={40} />
+            </a>
+            <a
+              href="https://www.facebook.com/profile.php?id=61569368545936"
+              className="text-yellow-300 hover:scale-110 transition-transform duration-300"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Visit our Facebook page"
+            >
+              <FaFacebook size={40} />
+            </a>
+          </div>
           <p className="mt-4 text-gray-300">
-
             <FaPhoneAlt className="inline-block ml-2 text-lg text-yellow-300" />
-            <strong>رقم الهاتف:</strong> <a href="https://wa.me/01211101073" target="_blank" rel="noopener noreferrer" className="inline-block mr-2 text-lg text-yellow-300" aria-label="WhatsApp link to 01211101073">01211101073</a>
+            <strong>رقم الهاتف:</strong>{' '}
+            <a
+              href="https://wa.me/01211101073"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline-block mr-2 text-lg text-yellow-300"
+              aria-label="WhatsApp link to 01211101073"
+            >
+              01211101073
+            </a>
           </p>
           <p className="text-gray-300">
-            📍 <strong>العنوان:</strong> <a href='https://maps.app.goo.gl/erAgwPeMpS2JMtZWA' target="_blank" className="inline-block mr-2 text-lg text-yellow-300"> قطعة 4 بلوك 12 بجوار المنارة للخضروات والفاكهة - منتجع النخيل، مدخل 2 </a>
+            📍 <strong>العنوان:</strong>{' '}
+            <a
+              href="https://maps.app.goo.gl/erAgwPeMpS2JMtZWA"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline-block mr-2 text-lg text-yellow-300"
+            >
+              قطعة 4 بلوك 12 بجوار المنارة للخضروات والفاكهة - منتجع النخيل، مدخل 2
+            </a>
           </p>
           <p className="text-gray-300">
-            📧 <strong>البريد الإلكتروني:</strong> <a href="mailto:citydevelopmentassociation@gmail.com" className="inline-block mr-2 text-lg text-yellow-300">citydevelopmentassociation@gmail.com</a>
+            📧 <strong>البريد الإلكتروني:</strong>{' '}
+            <a
+              href="mailto:citydevelopmentassociation@gmail.com"
+              className="inline-block mr-2 text-lg text-yellow-300"
+            >
+              citydevelopmentassociation@gmail.com
+            </a>
           </p>
         </div>
+
+        {/* Speed Links Section */}
+        <nav>
+          <h3 className="text-xl font-semibold text-center text-yellow-300 mb-4">روابط سريعة</h3>
+          <ul className="grid grid-cols-2 gap-4 justify-center">
+            <li>
+              <a
+                href="https://www.cairo.gov.eg/ar/Hay_AlSalamSecond/Pages/default.aspx"
+                className="text-yellow-300 hover:underline hover:scale-105 transition-transform duration-300"
+              >
+                حي ثان السلام
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.cairo.gov.eg/"
+                className="text-yellow-300 hover:underline hover:scale-105 transition-transform duration-300"
+              >
+                محافظة القاهرة
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://facebook.com/HayElsalam2/?locale=ar_AR"
+                className="text-yellow-300 hover:underline hover:scale-105 transition-transform duration-300"
+              >
+                الصفحة الرسمية لحي ثان السلام
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.shakwa.eg/GCP/Default.aspx"
+                className="text-yellow-300 hover:underline hover:scale-105 transition-transform duration-300"
+              >
+                بوابة الشكاوي الحكومية
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://lgs.gov.eg/"
+                className="text-yellow-300 hover:underline hover:scale-105 transition-transform duration-300"
+              >
+                بوابة خدمات المحليات
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://digital.gov.eg/"
+                className="text-yellow-300 hover:underline hover:scale-105 transition-transform duration-300"
+              >
+                بوابة مصر الرقمية
+              </a>
+            </li>
+          </ul>
+        </nav>
       </div>
 
-      {/* Social Media Section */}
+      {/* Footer Bottom Section */}
       <div className="text-center mt-8 text-gray-400">
         <p className="text-sm text-gray-300 mt-4">
-          © ٢٠٢٤ جمعية تنمية المدينة لخدمات البيئة - جميع الحقوق محفوظة
+          © {new Date().getFullYear()} جمعية تنمية المدينة لخدمات البيئة - جميع الحقوق محفوظة
         </p>
-        <p className="text-sm text-gray-300">
-          مشهر برقم: ١٢١٩٨ لسنة ٢٠٢٤
-        </p>
+        <p className="text-sm text-gray-300">مشهر برقم: ١٢١٩٨ لسنة ٢٠٢٤</p>
       </div>
     </footer>
   );
